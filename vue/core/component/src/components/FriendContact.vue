2d2265c2f0f69e6137cccc97a1b9eac616743074
diydriller <dhrhd080@naver.com>
2022-03-15 00:21:25 +0900

    ADD vue props

--- vue/core/component/src/components/FriendContact.vue
+++ vue/core/component/src/components/FriendContact.vue
@@ -1,6 +1,7 @@
 <template>
     <li>
-        <h2>{{name}}</h2>
+        <h2>{{name}} {{friendIsFavorite==='1'?'(Favorite)':''}}</h2>
+        <button @click="toggleFavorite">Toggle Favorite</button>
         <button @click="toggleDetails">{{detailsAreVisible?'Hide':'Show'}}</button>
         <ul v-if="detailsAreVisible">
             <li><strong>Phone:</strong>{{phoneNumber}}</li>
@@ -11,19 +12,45 @@
 
 <script>
 export default {
-    props:[
-        'name',
-        'phoneNumber',
-        'emailAddress'
-    ],
+    props:{
+        name:{
+            type:String,
+            required:true
+        },
+        phoneNumber:{
+            type:String,
+            required:true
+        },
+        emailAddress:{
+            type:String,
+            required:true
+        },
+        isFavorite:{
+            type:String,
+            required:false,
+            default:'0',
+            validator:function(value){
+                return value==='1'||value==='0';
+            }
+        }
+    },
     data(){
         return{
-            detailsAreVisible:false
+            detailsAreVisible:false,
+            friendIsFavorite:this.isFavorite
         }
     },
     methods:{
         toggleDetails(){
             this.detailsAreVisible=!this.detailsAreVisible;
+        },
+        toggleFavorite(){
+          if(this.friendIsFavorite==='0'){
+            this.friendIsFavorite='1';
+          }
+          else{
+            this.friendIsFavorite='0';
+          }
         }
     }
 }
