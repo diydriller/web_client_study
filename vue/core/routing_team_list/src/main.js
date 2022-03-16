ca16a1f510eac90a44c06dde7bf7afbe97a39a80
diydriller <dhrhd080@naver.com>
2022-03-16 17:41:28 +0900

    ADD vue router&vuex

---
+++ vue/core/routing_team_list/src/main.js
@@ -0,0 +1,29 @@
+import { createApp } from 'vue';
+import {createRouter, createWebHistory} from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList';
+import UsersList from './components/users/UsersList';
+import TeamMembers from './components/teams/TeamMembers';
+import NotFound from './components/nav/NotFound';
+
+const router=createRouter({
+  history:createWebHistory(),
+  routes:[
+    {path:'/',redirect:'/teams'},
+    {path:'/teams',
+      component:TeamsList,
+      children:[
+        {path:':teamId',component:TeamMembers,props:true}
+      ]
+    },
+    {path:'/users',component:UsersList},
+    // {path:'/teams/:teamId',component:TeamMembers,props:true},
+    {path:'/:notFound(.*)',component:NotFound}
+  ],
+  linkActiveClass:'active'
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
