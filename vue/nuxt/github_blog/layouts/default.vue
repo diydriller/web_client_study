ec6f37b31dbd5f33199086207107900b4034bc6e
diydriller <dhrhd080@naver.com>
2022-03-18 01:14:42 +0900

    ADD nuxt blog

---
+++ vue/nuxt/github_blog/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header/>
+    <div class="body">
+      <Category/>
+      <Nuxt/>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<style>
+.body{
+  display: flex
+}
+</style>
