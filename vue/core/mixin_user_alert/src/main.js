0541e8c10a061647be15c4aa95ab59da61a11df4
diydriller <dhrhd080@naver.com>
2022-03-24 17:19:34 +0900

    ADD composition api & hooks

---
+++ vue/core/mixin_user_alert/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import LoggerMixin from '@/mixins/logger';
+const app = createApp(App)
+
+app.mixin(LoggerMixin);
+app.mount('#app');
