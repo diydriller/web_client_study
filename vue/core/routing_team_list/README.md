93a8b8a9d89f7fdc9b4e487348bfda718ccd90ee
diydriller <dhrhd080@naver.com>
2022-04-13 17:22:49 +0900

    UPDATE vue>core>routing_team_list

---
+++ vue/core/routing_team_list/README.md
@@ -0,0 +1,2 @@
+## 1.Component
+![vue_core_routing](https://user-images.githubusercontent.com/41245313/163101238-fb75a6d2-09c3-4dd8-8f7d-cb7c4faf6aa4.png)
--- vue/core/routing_team_list/src/main.js
+++ vue/core/routing_team_list/src/main.js
@@ -6,23 +6,37 @@ import TeamsList from './components/teams/TeamsList';
 import UsersList from './components/users/UsersList';
 import TeamMembers from './components/teams/TeamMembers';
 import NotFound from './components/nav/NotFound';
+import TeamsFooter from './components/teams/TeamsFooter';
+import UsersFooter from './components/users/UsersFooter';
 
 const router=createRouter({
   history:createWebHistory(),
   routes:[
-    {path:'/',redirect:'/teams'},
-    {path:'/teams',
-      component:TeamsList,
+    { path:'/',redirect:'/teams'},
+    {
+      name: 'teams',
+      path:'/teams',
+      components:{ default:TeamsList , footer: TeamsFooter },
       children:[
-        {path:':teamId',component:TeamMembers,props:true}
+        { name: 'team-members',path:':teamId',component:TeamMembers,props:true}
       ]
     },
-    {path:'/users',component:UsersList},
-    // {path:'/teams/:teamId',component:TeamMembers,props:true},
-    {path:'/:notFound(.*)',component:NotFound}
+    { path:'/users',components: { default: UsersList, footer: UsersFooter }},
+    { path:'/:notFound(.*)',component:NotFound}
   ],
-  linkActiveClass:'active'
+  linkActiveClass:'active',
+  scrollBehavior(to,from,savedPosition){
+    if(savedPosition){
+      return savedPosition;
+    }
+    return { left:0,top:0};
+  }
 });
+
+router.beforeEach(function(to,from,next){
+  console.log(from,to);
+  next();
+})
 
 const app = createApp(App);
 app.use(router);
